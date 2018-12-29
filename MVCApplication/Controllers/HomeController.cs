9f6df5c7289fa5fb8fe1c5305a2105cc8000913c
mystified131 <mystifiedthomas@gmail.com>
2018-12-29 02:30:03 -0600

    Added instance search, sort and remove features.

--- MVCApplication/Controllers/HomeController.cs
+++ MVCApplication/Controllers/HomeController.cs
@@ -9,10 +9,12 @@ using MVCApplication.ViewModels;
 
 namespace MVCApplication.Controllers
 {
-  
+
     public class HomeController : Controller
     {
         public static List<Shape> TheList = new List<Shape>();
+        public static string Searchstr;
+        public static string Bridgeelement;
 
         public IActionResult Index()
         {
@@ -31,7 +33,9 @@ namespace MVCApplication.Controllers
         {
             ResultViewModel resultViewModel = new ResultViewModel();
 
-            resultViewModel.Error = "There is no value to asses the shape's attributes. Please return to the Input page.";
+            resultViewModel.Error = "To add a new shape, please return to the 'Add' page.";
+
+            resultViewModel.Shapelist = TheList;
 
             return View(resultViewModel);
         }
@@ -54,7 +58,7 @@ namespace MVCApplication.Controllers
 
                 return View(resultViewModel);
 
-                
+
             }
 
 
@@ -62,8 +66,135 @@ namespace MVCApplication.Controllers
 
         }
 
+
+        [HttpGet]
+        public IActionResult Remove()
+        {
+            if (TheList.Count > 0)
+            {
+                RemoveViewModel removeViewModel = new RemoveViewModel();
+
+                removeViewModel.TheList = TheList;
+
+                return View(removeViewModel);
+            }
+
+            else
+            {
+                return Redirect("/");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult Remove(RemoveViewModel removeViewModel)
+
+        {
+            if (ModelState.IsValid)
+            {
+
+                TheList.RemoveAll(x => x.Name == removeViewModel.NewElement1 & x.Sidelength == removeViewModel.NewElement2);
+
+                return Redirect("/Home/Result");
+            }
+
+            return Redirect("/Home/Error");
+
+        }
+
+
+        [HttpGet]
+        public IActionResult SearchSelect()
+        {
+            if (TheList.Count > 0)
+            {
+                SearchSelectViewModel searchSelectViewModel = new SearchSelectViewModel();
+
+                return View(searchSelectViewModel);
+            }
+
+            else
+            {
+                return Redirect("/");
+            }
+        }
+
+        [HttpPost]
+        public IActionResult SearchSelect(SearchSelectViewModel searchSelectViewModel)
+
+        {
+            if (ModelState.IsValid)
+
+            {
+                Searchstr = searchSelectViewModel.Searchstr.ToLower(); 
+                return Redirect("/Home/SearchResult");
+            }
+
+            return Redirect("/Home/Error");
+
+        }
+
+        [HttpGet]
+        public IActionResult SearchResult()
+        {
+            if (TheList.Count > 0)
+            {
+                SearchResultViewModel searchResultViewModel = new SearchResultViewModel();
+                List<Shape> Anslist = new List<Shape>();
+
+                foreach (Shape item in TheList)
+                {
+                    string itemname = item.Name.ToLower();
+
+                    if (itemname.Contains(Searchstr))
+                    {
+
+                        Anslist.Add(item);
+
+                    }
+
+
+                }
+
+                if (Anslist.Count == 0)
+                {
+                    Shape errshape = new Shape("That search returned no results", 0);
+
+                    Anslist.Add(errshape);
+
+                }
+
+                ViewBag.Anslist = Anslist;
+
+                return View(searchResultViewModel);
+            }
+
+            else
+            {
+                return Redirect("/");
+            }
+
+        }
+
+        [HttpGet]
+        public IActionResult Sort()
+        {
+            if (TheList.Count > 0)
+            {
+                SortViewModel sortViewModel = new SortViewModel();
+
+                List<Shape> Bridgelist = TheList.OrderBy(x => x.Sidelength).ToList();
+
+                sortViewModel.Sortlist = Bridgelist;
+
+                return View(sortViewModel);
+            }
+
+            else
+            {
+                return Redirect("/");
+            }
+        }
+
     }
-
-
 
 }
---
+++ MVCApplication/ViewModels/RemoveViewModel.cs
@@ -0,0 +1,17 @@
+﻿using MVCApplication.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MVCApplication.ViewModels
+{
+    public class RemoveViewModel
+    {
+        [Required]
+        public String NewElement1 { get; set; }
+        public double NewElement2 { get; set; }
+        public List<Shape> TheList { get; set; }
+    }
+}
